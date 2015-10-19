a4720a311fe76331704f1fdbd967ae8e0dc542b7
1204848 <j.c.1983@live.com>
2015-10-19 16:03:19 +0100

    <br />

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
 
         $myArray = array("do", "re", "mi");
         echo $myArray[0]; //outputs "do"
-        $myArray[1] = "la"; //modifies position 1 (re)
+        $myArray[1] = "la<br />"; //modifies position 1 (re)
         echo $myArray[1]; //outputs "la"
         unset($myArray[2]); //removes the array in position 2
 
